db1496450ac4aacdf824230f9a679d1e8e2c9c50
africa <africa@africa-PC>
2017-11-19 17:53:19 +0900

    initial commit

---
+++ src/com/smartpro/nova/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.smartpro.nova;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi, World!");
+		System.out.println("It's a beautiful day, isn't it?");
+	}
+
+}
