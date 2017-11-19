9ce81d40139e9d906d25febe8bde06de09dca39f
africa <africa@africa-PC>
2017-11-19 18:03:19 +0900

    update

--- src/com/smartpro/nova/HelloWorld.java
+++ src/com/smartpro/nova/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hi, World!");
 		System.out.println("It's a beautiful day, isn't it?");
+		System.out.println("This is to learn how to use Github");
 	}
 
 }
